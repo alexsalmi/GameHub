d20b306f47c5516cd264f3a2a09d24f98d630a1b
alexsalmi <alexjfsalmi@gmail.com>
2019-01-16 20:56:32 +0100

    Added Comments

--- Hangman/Hangman.cpp
+++ Hangman/Hangman.cpp
@@ -2,6 +2,7 @@
 
 //Constructor
 Hangman::Hangman(){
+	// Initializes each char in the pool as true, since they have not been guessed yet
 	for(char c : legalChars)
 		charPool[c] = true;
 }
@@ -22,6 +23,7 @@ void Hangman::start(){
 	}while(menuChoice!=0);
 }
 
+// Displays menu and takes input from user	
 int Hangman::menu(){
 	int menuChoice;
 	while(true){
@@ -54,20 +56,23 @@ int Hangman::menu(){
 	}
 }
 
+// Main game loop 
 void Hangman::play(){
 	char guess;
 	bool correct;
 	std::string spaces;
 	limbsRemain = 6;
+	// Reset the charPool before the game
 	for(char c : legalChars)
 		charPool[c] = true;
 
+	// Print the screen for Player 1 to enter the word/phrase to be guessed
 	printHeader();
 	ansi.textColor("green");
 	ansi.textAttr("bold");
-
 	cout << "               Player 1, please enter a word or phrase for Player 2 to try to guess:               \n"
 			"               ";
+	// Get the word or phrase. Using getline to include spaces
 	std::getline(std::cin >> std::ws, word);
 	// Error check for invalid input
 	if(cin.fail()){
@@ -75,33 +80,41 @@ void Hangman::play(){
     	cin.ignore();
 	}
 
+	// Keep track of how many letters need to be guessed
 	lettersToGuess = word.length();
 	for(char c: word){
 		if(c==' ' || c==',' || c=='.' || c=='!' || c=='?' || c=='-' || c=='\'')
 			lettersToGuess--;		
 	}
 
+	// Main game loop
 	while(limbsRemain>0){
 		printHeader();
 		ansi.textColor("green");
 		ansi.textAttr("bold");
 
+		// Prints the hanged man to the cmd window
 		printMan();
 
+		// Adjust the nember of spaced in front of the word so that the word is centered
 		spaces = "";
 		for(int i=0; i<(50-(word.length()/2)); i++)
 			spaces += " ";
 
+		// Print the word, showing only the letters that have been guessed, and any punctuation
 		cout << spaces;
 		for(char c: word){
 			if(c==' ' || c==',' || c=='.' || c=='!' || c=='?' || c=='-')
 				cout << c;
+			// Checking for capital letters
 			else if((c-'A')>=0 && (c-'A')<26){
+				// If the letter is in the charPool, it has not been guessed, so don't show it
 				if(charPool[c+32])
 					cout << '#';
 				else
 					cout << c;
 			}
+			// If the letter is in the charPool, it has not been guessed, so don't show it
 			else if(charPool[c])
 				cout << '#';
 			else
@@ -109,6 +122,7 @@ void Hangman::play(){
 		}
 		cout << "\n\n";
 
+		// Print all of the letters, unbolding the letters that have already been guessed
 		cout << "                        ";
 		for(char c : legalChars){
 			if(!charPool[c])
@@ -119,6 +133,7 @@ void Hangman::play(){
 
 		cout << "\n\n";
 
+		// Get the guessed letter from Player 2
 		cout << "                           Player 1, please choose a letter to guess: ";
 		cin >> guess;
 		// Error check for invalid input
@@ -126,11 +141,15 @@ void Hangman::play(){
 			cin.clear();
 	    	cin.ignore();
 		}
+		// Check for capital letter, change them to lower case
 		if((guess-'A')>=0 && (guess-'A')<26)
 			guess = guess + 32;
+		// If the letter is in the charPool, it has not been guessed, so we can continue
 		if(charPool[guess]){
+			// Mark the letter as being guessed
 			charPool[guess] = false;
 
+			// Check if the letter is in the word/phrase. If it is, update lettersToGuess for each occurance
 			correct = false;
 			for(char c: word){
 				if(c==guess || (c+32)==guess){
@@ -139,13 +158,16 @@ void Hangman::play(){
 				}
 			}
 
+			// If it was an incorrect guess, hang another limb
 			if(!correct)
 				limbsRemain--;
 
+			// If there are no limbs left, the game is over, show endgame screen
 			if(limbsRemain==0){
 				endgame(false);
 				return;
 			}
+			// If the word has been fully guessed, the game is over, show endgame screen
 			else if(lettersToGuess==0){
 				endgame(true);
 				return;
@@ -154,6 +176,7 @@ void Hangman::play(){
 	}
 }
 
+// Displays post game screen
 void Hangman::endgame(bool didGuess){
 	printHeader();
 	ansi.textColor("green");
@@ -171,6 +194,7 @@ void Hangman::endgame(bool didGuess){
 	ansi.textReset();
 }
 
+// Prints the figure of the hanged man
 void Hangman::printMan(){
 	cout << "                                        XXXXXXXXXX           \n"
 			"                                        X        X           \n";
--- Hangman/Hangman.h
+++ Hangman/Hangman.h
@@ -21,19 +21,21 @@ private:
 	int menu();
 	// Main game loop 
 	void play();
-	// Prints the hanged man
-	void printMan();
 	// Displays post game screen
 	void endgame(bool didGuess);
+	// Prints the hanged man
+	void printMan();
 	// Prints memory header to cmd window
 	void printHeader();	
 	// Displays the rules of the game
 	void rules();
 
-	int limbsRemain = 6, lettersToGuess;
-	std::string legalChars = "abcdefghijklmnopqrstuvwxyz";
-	unordered_map<char, bool> charPool;
-	string word;
+	int limbsRemain = 6, lettersToGuess; 		// To keep track of limbs and letters in the word/phrase
+	std::string legalChars = "abcdefghijklmnopqrstuvwxyz";	// Alphabet
+	unordered_map<char, bool> charPool;			// To keep track of which letters have been guessed
+	string word;								// For the inputted word/phrase from Player 1
+
+	// For ansi escape commands
 	dispANSI ansi;
 };
 
